df346adc61af793b9f7488176ad1cac9d2b53642
Nivedita Chatterjee <niveditabarn21@gmail.com>
2022-08-08 13:23:26 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+const Header = () =>{
+    return(
+        <div className='holder'>
+            <header className="header">
+                <Navbar />
+            <div className="header-content flex flex-c text-center text-white">
+<h2 className="header-title text-capitalize">
+    Find Books of Your Choice !
+</h2>
+<p className="header-text fs-18 fw-3">
+   Welcome to Book Finder, a website having the largest collection of books from authors from all corners of the world. Just dive into our collection and find books of your favourite authors and genre anytime !
+</p>
+<SearchForm />
+</div>
+             </header>
+           
+        </div>
+    )
+}
+export default Header;
